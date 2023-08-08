5ba0143e61153187c66f87b7e1ba48a06ab03daf
Eddie Sandler <ed.sandler@gmail.com>
2023-08-08 16:27:02 -0400

    WIP

--- js-array-reduce/reduce.js
+++ js-array-reduce/reduce.js
@@ -2,62 +2,105 @@
 Write a function called extractValue which accepts an array of objects and a key and returns a new array with the value of each object at the key.
 
 Examples:
-    const arr = [{name: 'Elie'}, {name: 'Tim'}, {name: 'Matt'}, {name: 'Colt'}]
-    extractValue(arr,'name') // ['Elie', 'Tim', 'Matt', 'Colt']
 */
+// const arr = [{name: 'Elie'}, {name: 'Tim'}, {name: 'Matt'}, {name: 'Colt'}]
+// extractValue(arr,'name') // ['Elie', 'Tim', 'Matt', 'Colt']
 
-function extractValue(arr, key) {}
+
+function extractValue(arr, key) {
+
+    return arr.reduce((accumulator, currentVal) => {
+        accumulator.push(currentVal[key]);
+        return accumulator;
+
+    }, []);
+
+}
 
 /*
 Write a function called vowelCount which accepts a string and returns an object with the keys as the vowel and the values as the number of times the vowel appears in the string. This function should be case insensitive so a lowercase letter and uppercase letter should count
 
-Examples:
-    vowelCount('Elie') // {e:2,i:1};
-    vowelCount('Tim') // {i:1};
-    vowelCount('Matt') // {a:1})
-    vowelCount('hmmm') // {};
-    vowelCount('I Am awesome and so are you') // {i: 1, a: 4, e: 3, o: 3, u: 1};
-*/
+Examples:*/
+//vowelCount('Elie'); // {e:2,i:1};
+// vowelCount('Tim'); // {i:1};
+// vowelCount('Matt'); // {a:1})
+// vowelCount('hmmm'); // {};
+// vowelCount('I Am awesome and so are you'); // {i: 1, a: 4, e: 3, o: 3, u: 1};
 
-function vowelCount(str) {}
+
+function vowelCount(str) {
+    const vowels = 'aeiou';
+    str = str.toLowerCase();
+    const phrase = Array.from(str);
+    return phrase.reduce((accumulator, currentVal) => {
+        //if the current value is a vowel and it doesnt exist in the accumulator object , add as key to the object with value of 1
+        //else-if the current values is  a vowel and is in the object, increment the key's  value
+        //return accumulator object
+
+        if (vowels.indexOf(currentVal) > -1 && accumulator[currentVal] === undefined){
+            accumulator[currentVal] = 1;
+        }
+        else if (vowels.indexOf(currentVal) > -1 && accumulator[currentVal] !== undefined){
+            accumulator[currentVal] +=1;
+        }
+
+       return accumulator;
+
+    }, {});
+
+
+}
 
 /*
 Write a function called addKeyAndValue which accepts an array of objects and returns the array of objects passed to it with each object now including the key and value passed to the function.
 
-Examples:
+Examples:*/
     const arr = [{name: 'Elie'}, {name: 'Tim'}, {name: 'Matt'}, {name: 'Colt'}];
-    
-    addKeyAndValue(arr, 'title', 'Instructor') // 
+
+    addKeyAndValue(arr, 'title', 'Instructor') //
       [
-        {title: 'Instructor', name: 'Elie'}, 
-        {title: 'Instructor', name: 'Tim'}, 
-        {title: 'Instructor', name: 'Matt'}, 
+        {title: 'Instructor', name: 'Elie'},
+        {title: 'Instructor', name: 'Tim'},
+        {title: 'Instructor', name: 'Matt'},
         {title: 'Instructor', name: 'Colt'}
        ]
-*/
 
-function addKeyAndValue(arr, key, value) {}
+
+function addKeyAndValue(arr, key, value) {
+    return arr.reduce((accumulator,currentVal,index)=>{
+
+        accumulator[index][key]=value
+
+        return accumulator
+
+    },arr)
+ }
+
+
+
+
+
 
 /*
-Write a function called partition which accepts an array and a callback and returns an array with two arrays inside of it. The partition function should run the callback function on each value in the array and if the result of the callback function at that specific value is true, the value should be placed in the first subarray. If the result of the callback function at that specific value is false, the value should be placed in the second subarray. 
+Write a function called partition which accepts an array and a callback and returns an array with two arrays inside of it. The partition function should run the callback function on each value in the array and if the result of the callback function at that specific value is true, the value should be placed in the first subarray. If the result of the callback function at that specific value is false, the value should be placed in the second subarray.
 
 Examples:
-    
+
     function isEven(val){
         return val % 2 === 0;
     }
-    
+
     const arr = [1,2,3,4,5,6,7,8];
-    
+
     partition(arr, isEven) // [[2,4,6,8], [1,3,5,7]];
-    
+
     function isLongerThanThreeCharacters(val){
         return val.length > 3;
     }
-    
+
     const names = ['Elie', 'Colt', 'Tim', 'Matt'];
-    
+
     partition(names, isLongerThanThreeCharacters) // [['Elie', 'Colt', 'Matt'], ['Tim']]
 */
 
-function partition(arr, callback) {}
+function partition(arr, callback) { }
